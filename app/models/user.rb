058a99d053679a378e4570ed58f927bf5431600a
Daniel Torres <denial.torres@gmail.com>
2021-12-24 00:00:02 -0600

    dependency injection concept

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,20 @@ class User < ApplicationRecord
 
   has_many :notifications, foreign_key: :recipient_id
   has_many :services
+
+  def geocode(service: GoogleMapsAPI)
+    lat, lng = service.geocode(address)
+
+    update(
+      latitude: lat,
+      longitude: lng
+    )
+  end
+end
+
+
+class FakeMapsAPI
+  def self.geocode(address)
+    [1,1]
+  end
 end
